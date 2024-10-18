cd32e74935900ae8f2a854c7fef5e48640fc6dd8
student <kooduser@pupil.kood.tech>
2024-10-18 18:33:33 +0300

    ver 1.01

---
+++ sprint/AtBashTool.java
@@ -0,0 +1,14 @@
+package sprint;
+
+public class AtBashTool{
+
+
+    public String encryptAtBash(String input){
+
+    }
+
+    public String decryptAtBash(String input){
+
+    }
+    
+}
---
+++ sprint/Rot13Tool.java
@@ -0,0 +1,18 @@
+package sprint;
+
+public class Rot13Tool{
+
+    //метод для шифровки
+    public String encryptRot13(String input){
+
+    }
+
+    //метод для дешрофки
+    public String decryptRot13(String input){
+
+    }
+    
+}
+
+
+// если что спрашивай ;)
